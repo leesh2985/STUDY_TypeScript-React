2be625485acc69df48cb378c06976e4dd8c1ba37
Seung Lee <leesh2985@naver.com>
2023-06-01 20:51:46 +0900

    Tab 만들기

--- github-lssue/src/Header.js
+++ github-lssue/src/Header.js
@@ -2,39 +2,43 @@ import styles from "./Header.module.css";
 
 import Button from "./components/Button";
 import Space from "./components/Space";
+import Tabs from "./components/Tabs";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <Button
-        style={{
-          fontSize: "14px",
-          backgroundColor: "transparent",
-          color: "black",
-        }}
-      >
-        Watch
-      </Button>
-      <Space />
-      <Button
-        style={{
-          fontSize: "14px",
-          backgroundColor: "transparent",
-          color: "black",
-        }}
-      >
-        Fork <div className={styles.circle}>5</div>
-      </Button>
-      <Space />
-      <Button
-        style={{
-          fontSize: "14px",
-          backgroundColor: "transparent",
-          color: "black",
-        }}
-      >
-        Star
-      </Button>
+      <div className={styles.buttonContainer}>
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Watch
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Fork <div className={styles.circle}>5</div>
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Star
+        </Button>
+      </div>
+      <Tabs />
     </div>
   );
 }
