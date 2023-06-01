fba0702f57d82f44ae69049ec97ec41e64a45391
Seung Lee <leesh2985@naver.com>
2023-06-01 23:43:08 +0900

    모달 인풋 설정

--- github-lssue/src/components/Modal.js
+++ github-lssue/src/components/Modal.js
@@ -1,12 +1,15 @@
 import styles from "./Modal.modul.css";
 import cx from "clsx";
 
-export default function Modal({ opened, title, onClose }) {
+export default function Modal({ opened, title, onClose, placeholder }) {
   return (
     <div className={cx(styles.modal, { [styles.opened]: opened })}>
       <div className={styles.header}>
         <span>{title}</span>
         <button onClick={onClose}>X</button>
+      </div>
+      <div className={styles.input}>
+        <input placeholder={placeholder} />
       </div>
     </div>
   );
