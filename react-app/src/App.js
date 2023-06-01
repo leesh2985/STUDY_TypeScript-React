d498835d6e88b5565ed98e864af0d1bdb9eb30e9
Seung Lee <leesh2985@naver.com>
2023-06-01 17:03:48 +0900

    훅 설명

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,10 +10,20 @@ import React, { useEffect, useState } from "react";
  */
 
 function App() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
 
-  useEffect(() => console.log("Hello"), [count]);
-  return <div className="App"></div>;
+  useEffect(() => {
+    if (count === 5) {
+      console.log("this is five");
+    }
+  }, [count]);
+
+  return (
+    <div className="App">
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 }
 
 export default App;
